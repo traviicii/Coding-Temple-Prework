4fb96cab5d9eb14066de8ce2e961980d28bddae9
traviicii <125103256+traviicii@users.noreply.github.com>
2023-02-15 01:30:20 -0500

    Added gradient to main head along with other minor updates
    -Changed off canvas panel exit button to a minus instead of an X
    -comment adjustments

--- portfolio/java.js
+++ portfolio/java.js
@@ -6,7 +6,7 @@ function openNav() {
     /*document.body.style.backgroundColor = "rgba(0,0,0,0.4)";*/
   }
   
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
