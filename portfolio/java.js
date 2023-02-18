862deb83915b42e23b9f3f697e35186cdd1cee91
traviicii <125103256+traviicii@users.noreply.github.com>
2023-02-17 21:25:31 -0500

    quick update

--- portfolio/java.js
+++ portfolio/java.js
@@ -1,8 +1,8 @@
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "175px";
-    document.getElementById("main").style.marginLeft = "175px";
+    document.getElementById("mySidenav").style.width = "210px";
+    document.getElementById("main").style.marginLeft = "210px";
     /*document.body.style.backgroundColor = "rgba(0,0,0,0.4)";*/
   }
   
